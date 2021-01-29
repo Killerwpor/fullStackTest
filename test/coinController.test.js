55ffe183051747a726e40041159173e32810cb2f
Johan Suárez <johanc.suarez@hotmail.com>
2021-01-29 10:34:29 -0500

    Feature: test

--- test/coinController.test.js
+++ test/coinController.test.js
@@ -4,8 +4,6 @@ const controlador = require("../controllers/coinController");
 test('nombre del test', async () => {
 
   const monedasPrueba=["Bitcoin","Ethereum","Litecoin"];
-  // const monedas=await controlador.obtenerInfoMonedas(monedasPrueba);
-  // console.log(monedas);
   const monedas=await  controlador.obtenerInfoMonedas(monedasPrueba);
   expect(monedas).toHaveLength(3);
 
