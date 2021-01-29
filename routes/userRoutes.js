77f8e7db4844b99925bf0007a31a47cd6cdf49ac
Johan Suarez <johanc.suarez@hotmail.com>
2021-01-29 16:00:38 -0500

    Feature: tests done

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,13 +5,10 @@ const express = require("express");
 const router = express.Router();
 
 //Routes
-router.get("/", authValidator.validateToken, controlador.traerUsuarios);
 
 router.post("/guardarUsuario", controlador.guardarUsuario);
 
 router.post("/login", controlador.login);
-
-router.post("/guardarMoneda", controlador.guardarMoneda);
 
 router.get("/listarMonedas", controlador.listarMonedas);
 
