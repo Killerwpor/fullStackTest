c5e848fb3d953d74152833c9fb0c679435974d80
Johan Suarez <johanc.suarez@hotmail.com>
2021-02-03 12:25:17 -0500

    Fix: sequelize conections in test

--- test/userController.test.js
+++ test/userController.test.js
@@ -132,3 +132,45 @@ test("Listar monedas sin permiso", async (done) => {
 
   await controlador.login(req, res);
 });
+
+const monedasPrueba = ["Bitcoin", "Ethereum", "Litecoin"];
+
+test("Retorna info para cada moneda", async () => {
+  const monedas = await controladorCoin.obtenerInfoMonedas(monedasPrueba);
+  expect(monedas).toHaveLength(3);
+});
+
+test("Retorna la info que es", async () => {
+  const monedas = await controladorCoin.obtenerInfoMonedas(monedasPrueba);
+  expect(monedas).toContainEqual(
+    {
+      id: "f1ff77b6-3ab4-4719-9ded-2fc7e71cff1f",
+      name: "Bitcoin",
+      symbol: "BTC",
+    },
+    {
+      id: "e991ba77-d384-48ff-b0a4-40e95ef6b7d6",
+      name: "Ethereum",
+      symbol: "ETH",
+    },
+    {
+      id: "e0c280b3-b4cf-4012-9e08-14a816ef2c9c",
+      name: "Litecoin",
+      symbol: "LTC",
+    }
+  );
+});
+
+test("Trae el numero de precios correctos conversión monedas", async () => {
+  const monedaFavorita = ["Bitcoin"];
+  const conversionMonedas = await controladorCoin.conversionMonedas(
+    monedasPrueba,
+    monedaFavorita
+  );
+  expect(conversionMonedas).toHaveLength(3);
+}, 20000);
+
+test("Obtiene un token", async () => {
+  const token = await controladorCoin.getToken();
+  expect(token).toEqual(expect.any(String));
+});
